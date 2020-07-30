c67ad32eafbe87d389f0e66c39170a7a80ffe0a3
David Gardiner <dagardin@microsoft.com>
2020-07-30 11:34:41 -0700

    Update report

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -20,7 +20,7 @@ function Landing() {
         <br/>
         <br/>
         <div style={dashboardStyle}>
-          <iframe width='100%' height='100%' src='https://msit.powerbi.com/reportEmbed?reportId=0ece3a94-1517-45f8-b25f-8e81fb718e05&autoAuth=true&ctid=72f988bf-86f1-41af-91ab-2d7cd011db47&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9kZi1tc2l0LXNjdXMtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D' allowFullScreen={true} ></iframe>
+          <iframe width='100%' height='100%' src='https://msit.powerbi.com/reportEmbed?reportId=542a499b-0e2c-4e73-b7fa-3efe8080b373&autoAuth=true&ctid=72f988bf-86f1-41af-91ab-2d7cd011db47&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9kZi1tc2l0LXNjdXMtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D' allowFullScreen={true} ></iframe>
         </div>
     </Wrapper>
   );
